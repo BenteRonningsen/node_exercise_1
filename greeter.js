df153d4fdaf2a366a229d43ef8c88a456b7f9759
unknown <bentvr15@uia.no>
2017-10-04 16:53:10 +0200

    test

---
+++ greeter.js
@@ -0,0 +1,10 @@
+var Greeting = 5;
+var randomCount = Math.round(Math.random() * (Greeting - 1)) + 1;
+var greeting = new Array();
+greeting[1] = "Hello",
+greeting[2] = "Hei",
+greeting[3] = "Hola",
+greeting[4] = "Halla",
+greeting[5] = "Good Day",
+
+console.log(greeting[randomCount]);
